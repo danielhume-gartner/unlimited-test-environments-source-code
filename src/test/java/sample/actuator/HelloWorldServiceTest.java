83c34f2d0e8969426403a72fb20312d81c7a1191
danielhume-gartner <103504612+danielhume-gartner@users.noreply.github.com>
2022-05-23 15:23:10 +0200

    Update tests for testbranch

--- src/test/java/sample/actuator/HelloWorldServiceTest.java
+++ src/test/java/sample/actuator/HelloWorldServiceTest.java
@@ -9,7 +9,7 @@ public class HelloWorldServiceTest {
 	@Test
 	public void expectedMessage() {
 		HelloWorldService helloWorldService = new HelloWorldService();
-		assertEquals("Expected correct message","Spring boot says hello from a Docker container deployed by codefresh",helloWorldService.getHelloMessage());
+		assertEquals("Expected correct message","This web service is running from branch: testbranch",helloWorldService.getHelloMessage());
 	}
 	
 }
